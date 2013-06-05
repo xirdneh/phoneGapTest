5c18ffa7be27c5bd626af2ad276832ed2ede496b
xirdneh <xirdneh@gmail.com>
2013-06-05 15:44:00 -0500

    Scrolling Test

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert("Initialization!!", "Info");
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
     
     showAlert: function (message, title){
@@ -27,7 +13,7 @@ var app = {
         } else {
             alert( title ? (title + ": " + message) : message);
         }
-    }
+    }   
 };
 
 app.initialize();
