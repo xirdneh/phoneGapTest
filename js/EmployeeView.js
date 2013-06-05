e82745b4138488310c7ec1d8c2fa743573d89721
xirdneh <xirdneh@gmail.com>
2013-06-05 17:06:05 -0500

    Added GeoLocation

---
+++ js/EmployeeView.js
@@ -0,0 +1,23 @@
+var EmployeeView = function(employee) {
+    this.render = function(){
+        this.el.html(EmployeeView.template(employee));
+        return this;
+    };
+    this.addLocation = function(event){
+        event.preventDefault();
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+            },
+            function() {
+                alert('Error getting location');
+            });
+        return false;
+    };
+    this.initialize = function() {
+        this.el = $('<div/>');
+        this.el.on('click', '.add-location-btn', this.addLocation);
+    };
+    this.initialize();
+}
+EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ var app = {
 
     initialize: function() {
         var self = this;
-        this.store = new MemoryStore(function() {
-            $('body').html(new HomeView(self.store).render().el);
-        });
+        this.detailsURL = /^#employees\/(\d{1,})/;
         this.registerEvents();
+        this.store = new MemoryStore(function() {
+            self.route();
+        });
     },
     
     showAlert: function (message, title){
@@ -13,6 +14,20 @@ var app = {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert( title ? (title + ": " + message) : message);
+        }
+    },
+    
+    route: function(){
+        var hash = window.location.hash;
+        if (!hash){
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
         }
     },
 
@@ -33,6 +48,7 @@ var app = {
                 $(event.target).removeClass('tappable-active');
             });
         }
+        $(window).on('hashchange', $.proxy(this.route, this));
     }
 };
 
